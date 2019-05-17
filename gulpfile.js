f54d697090b4b0ad138d4a514622fad6ce31a618
随便用户名 <>
2019-05-17 08:59:12 +0800

    hello emy

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require("gulp");
+const webserver = require("gulp-webserver");
+const express = require("express");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const csso = require("gulp-csso");
+const autoprefixer = require("gulp-autoprefixer")
+
+gulp.task("compileJS", ()=>{
+	gulp.src("src/scripts/**/*.js")
+		.pipe( babel({
+			presets : ["@babel/env"]
+		}) )
+		.pipe( uglify() )
+		.pipe( gulp.dest("dist/scripts") )
+	gulp.src("src/pages/**/*.js")
+		.pipe( babel({
+			presets : ["@babel/env"]
+		}) )
+		.pipe( uglify() )
+		.pipe( gulp.dest("dist/pages") )
+	gulp.src("src/static/**/*").pipe( gulp.dest("dist/static") );
+})
+gulp.task("compileCSS", ()=>{
+	gulp.src("src/styles/**/*.scss")
+		.pipe( sass() )
+		.pipe( autoprefixer() )
+		.pipe( csso() )
+		.pipe( gulp.dest("dist/styles") )
+})
+gulp.task("compileHTML", ()=>{
+	gulp.src("src/pages/**/*.html")
+		.pipe( gulp.dest("dist/pages") )
+})
+
+
+gulp.task("server", function(){
+	//静态资源服务器 : 9999
+	gulp.src("dist")
+		.pipe( webserver({
+			livereload : true,
+			port : 9999
+		}) )
+	gulp.watch("src/pages/**/*.js", ["compileJS"]);
+	gulp.watch("src/scripts/**/*.js", ["compileJS"]);
+	gulp.watch("src/styles/**/*.scss", ["compileCSS"]);
+	gulp.watch("src/pages/**/*.html", ["compileHTML"])
+	
+	//接口代理服务器
+	let app = express();
+	app.get("./home", (req,res)=>{
+		res.setHeader("Access-Control-Allow-Origin","*"); //cors
+		res.setHeader("Content-Type","text/plain; charset=utf8")
+		let proxy = https.request({
+			hostname: "www.smartisan.com",
+			path: "/product/shop_categories",
+			method: 'get'
+		}, (response) => {
+			response.pipe(res);
+		});
+		proxy.end();
+	})
+	app.listen(8000);
+})
+
+
+gulp.task("build", ["compileJS","compileCSS","compileHTML"])
