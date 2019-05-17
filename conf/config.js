46407fba0603cc6d8319924487c94f4a40541fea
随便用户名 <>
2019-05-17 09:05:57 +0800

    niyinbgle

---
+++ conf/config.js
@@ -0,0 +1,26 @@
+requirejs.config({
+	baseUrl : "file:///c:/Users/Administrator/Desktop/requireJS9090/",
+	paths : {
+		"vali" : "scripts/jquery.validate",
+		"jquery" : "scripts/jquery-2.0.3",
+		"sw" : "scripts/swiper",
+		"jq.cookie" : "scripts/jquery.cookie",
+		"bootstrap" : "scripts/bootstrap",
+		"jquery.ui" : "scripts/jquery-ui",
+		"css" : "scripts/css"  //加载CSS文件的插件
+	},
+	shim : {
+		"jq.cookie" : {
+			deps : ["jquery"]
+		},
+		"sw" : {
+			deps : ["css!styles/swiper.css"]
+		},
+		"bootstrap" : {
+			deps : [
+				"jquery",
+				"css!styles/bootstrap.css"
+			]
+		}
+	}
+})
